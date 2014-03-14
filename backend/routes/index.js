09b4f55ee50abcb5c1b8b1a7c5b0c1da5b10b584
Christoph Hermann <schtoeffel@gmail.com>
2014-03-14 14:24:08 +0100

    asd

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,8 +1,98 @@
+var mongoose = require('mongoose'),
+  config = require('../config.json'),
+  Q = require('q'),
+  md5 = require('MD5'),
+  nodemailer = require('nodemailer'),
+  Registration;
 
-/*
- * GET home page.
- */
+connect();
 
-exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+exports.register = function(req, res) {
+  var hash, registration;
+  hash = md5('asdjkhads891k12' + req.body.name + req.body.email + 'ajvghbr82123h;');
+  registration = new Registration({
+    name: req.body.name,
+    lastname: req.body.lastname,
+    birthday: req.body.birthday,
+    comment: req.body.comment,
+    adress: req.body.adress,
+    email: req.body.email,
+    confirmed: false,
+    hash: hash
+  });
+  registration.save(function(err, registration) {
+    if (err) {
+      res.send(500, 'Schon vergessen? Du bist bereits angemeldet');
+    } else {
+      if (config.sendEmail) {
+        sendMail(req.body.email, hash);
+      }
+      res.send(req.body.email);
+    }
+  });
 };
+
+exports.confirm = function(req, res) {
+  console.log('confirm');
+};
+
+
+function connect() {
+  var q = Q.defer();
+  mongoose.connect('mongodb://localhost/lanCamp');
+  var db = mongoose.connection;
+  db.on('error', function() {
+    console.log(arguments);
+    q.reject();
+  });
+  db.once('open', function callback() {
+    createSchema();
+    q.resolve();
+  });
+  return q.promise;
+}
+
+function createSchema() {
+  var schema = mongoose.Schema({
+    name: String,
+    lastname: String,
+    birthday: String,
+    comment: String,
+    adress: String,
+    email: String,
+    confirmed: Boolean,
+    hash: String
+  });
+  schema.path('email').index({
+    unique: true
+  });
+  Registration = mongoose.model('Registration', schema);
+}
+
+function sendMail(email, hash) {
+  var smtpTransport = nodemailer.createTransport('SMTP', {
+    service: 'Gmail',
+    auth: {
+      user: config.email,
+      pass: config.password
+    }
+  }),
+    mailOptions = {
+      from: 'LanCamp ✔ <registration@lan-camp.ch>', // sender address
+      to: email,
+      subject: 'LanCamp 14 - Anmeldungbestätigen ✔', // Subject line
+      html: 'Bitte bestätige deine Anmeldung mit einem click auf den Link unten. <a href=\'http://127.0.0.1:3000/confirm/' + hash + '\'>lan-camp.ch/confirm/' + hash + '</a>'
+    };
+
+  // send mail with defined transport object
+  smtpTransport.sendMail(mailOptions, function(error, response) {
+    if (error) {
+      console.log(error);
+    } else {
+      console.log('Message sent: ' + response.message);
+    }
+
+    // if you don't want to use this transport object anymore, uncomment following line
+    //smtpTransport.close(); // shut down the connection pool, no more messages
+  });
+}
